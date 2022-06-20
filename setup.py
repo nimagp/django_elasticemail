40b7816c4454b897d64adbb00200f5e870163dd0
Nima Ghasemi Por <71447191+nimagp@users.noreply.github.com>
2022-06-20 22:01:53 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ import setuptools
 setuptools.setup(name="django_elasticemail",
 version="0.0.1",
 packages=["django_elasticemail"],
-install_requires= ["six","requests"]
+install_requires= ["six","requests"],
 python_requires=">=3.6")
